c5b1015999c61e96555aa9c555ca5b0811a81a58
Hasan YILMAZ <hsnylmz@gmail.com>
2023-07-30 23:20:55 +0300

    getting better

--- razor.py
+++ razor.py
@@ -13,7 +13,7 @@ from ttkbootstrap.constants import *
 from tkinter import filedialog, Tk, Button
 
 window = ttk.Window(title="Razor by hsnylmz", themename="litera")
-window.geometry("430x280")
+window.geometry("520x280")
 window.resizable(True,True)
 
 try:
@@ -26,6 +26,7 @@ source_video_path=""
 target_video_file=""
 file_ext=""
 videos_folder = os.path.join(os.path.expanduser("~"), "Videos")
+yolumuz=os.getcwd()
 
 ### frame rate alınması lazım...
 def get_frame_rate(source_video_path):
@@ -69,15 +70,16 @@ def cevir():
     target_video_file = 'output_' + source_file_name
     print('Target File Name : '+target_video_file)
 
-    label_result.config(text=f"Seçilen Video: {source_file_name}")
+    secilen_video_label.config(text=f"Seçilen Video: {source_file_name}", font=("Arial", 14))
+    secilen_video_frame_rate_label.config(text=f"Frame Rate : {fps}", font=("Arial", 14))
 
     olarak_kaydet_button.config(state=NORMAL)
-    convert_button.config(state=DISABLED)
+    #convert_button.config(state=DISABLED)
        
 
 def farkli_kaydet():
     
-    global file_ext, source_file_name
+    global file_ext, source_file_name, yolumuz
     
     tc1=get_tc_1()
     tc_1=tc1.strip()
@@ -90,7 +92,7 @@ def farkli_kaydet():
     dest_file_path = filedialog.askdirectory()
     print('dest_file_path : '+dest_file_path)
 
-    yolumuz=os.getcwd()
+    
     convert_command='{}/ffmpeg.exe -ss {} -i "{}" -c copy -t {} "{}/{}"'.format(yolumuz, tc_1, source_video_path, tc_2, dest_file_path, target_video_file)
     print('-')
     print('burası senin son uğraştığın yer : '+convert_command)
@@ -102,9 +104,9 @@ def farkli_kaydet():
     os.chdir(dest_file_path)    
     print("Burası çıkış yeri şu an : "+dest_file_path)
 
-    yolumuz=os.getcwd()
-    print("Burası da şu an bulunduğumuz yer : "+yolumuz)
-    subprocess.call('explorer ' + yolumuz + ', shell=True')
+    yolumuz_=os.getcwd()
+    print("Burası da şu an bulunduğumuz yer : "+yolumuz_)
+    subprocess.call('explorer ' + yolumuz_ + ', shell=True')
     
     #biten videoyu aç
     os.chdir(dest_file_path)
@@ -126,37 +128,46 @@ def get_tc_2():
 
 # USER INTERFACE
 label_frame = ttk.LabelFrame(window, text="İşleminizi Seçiniz : ")
-label_frame.pack(padx=10, pady=10, fill=ttk.BOTH, expand=True)
+label_frame.pack(padx=5, pady=5)
 
-convert_button = ttk.Button(label_frame, text="VİDEOYU SEÇİNİZ", command=cevir, width=25, bootstyle=SUCCESS)
+label_frame_2 = ttk.LabelFrame(window, text="Video Özellikleri ")
+label_frame_2.pack(padx=5, pady=5, fill=ttk.BOTH, expand=True)
+
+head_label_tc1 = ttk.Label(label_frame, text="TC IN")
+head_label_tc1.grid(row=0, column=1, padx=5, pady=5)
+
+head_label_tc2 = ttk.Label(label_frame, text="TC DURATION")
+head_label_tc2.grid(row=0, column=2, padx=5, pady=5)
+
+
+convert_button = ttk.Button(label_frame, text="VİDEOYU SEÇİNİZ", command=cevir, width=15, bootstyle=SUCCESS)
 #convert_button.pack(side='left', padx=5, pady=5)
-convert_button.grid(row=0, column=0, padx=5, pady=5)
+convert_button.grid(row=1, column=0, padx=5, pady=5)
 
-label_result = ttk.Label(label_frame, text="")
-label_result.grid(row=1, columnspan=2, padx=5, pady=5)
-
-label_tc1=ttk.Label(label_frame,text="TIMECODE IN : ")
-#label_tc1.pack(side='left', padx=5, pady=5)
-label_tc1.grid(row=2, column=0, padx=5, pady=5)
-
-text_box_tc_1 = ttk.Text(label_frame, height=1, width=10)
+text_box_tc_1 = ttk.Text(label_frame, height=1, width=10,font=("Arial", 14))
 text_box_tc_1.insert("1.0", "00:00:00.0")
 #text_box_tc_1.pack(side='left', padx=5, pady=5)
-text_box_tc_1.grid(row=2, column=1, padx=5, pady=5)
+text_box_tc_1.grid(row=1, column=1, padx=5, pady=5)
 
-label_tc2=ttk.Label(label_frame,text="TIMECODE DURATION : ")
-#label_tc2.pack(side='left', padx=5, pady=5)
-label_tc2.grid(row=3, column=0, padx=5, pady=5)
+#label_tc2=ttk.Label(label_frame,text="TIMECODE DURATION : ")
+#label_tc2.grid(row=4, column=0, padx=5, pady=5)
 
-text_box_tc_2 = ttk.Text(label_frame, height=1, width=10)
+text_box_tc_2 = ttk.Text(label_frame, height=1, width=10,font=("Arial", 14))
 text_box_tc_2.insert("1.0", "00:00:10.0")
 #text_box_tc_2.pack(side='left', padx=5, pady=5)
-text_box_tc_2.grid(row=3, column=1, padx=5, pady=5)
+text_box_tc_2.grid(row=1, column=2, padx=5, pady=5)
 
-olarak_kaydet_button = ttk.Button(label_frame, text="K A Y D E T", command=farkli_kaydet, width=25, bootstyle=SUCCESS)
+olarak_kaydet_button = ttk.Button(label_frame, text="K A Y D E T", command=farkli_kaydet, width=15, bootstyle=SUCCESS)
 olarak_kaydet_button.config(state=DISABLED)
 #olarak_kaydet_button.pack(side='left', padx=5, pady=5)
-olarak_kaydet_button.grid(row=4, column=0, padx=5, pady=5)
+olarak_kaydet_button.grid(row=1, column=3, padx=5, pady=5)
+
+secilen_video_label = ttk.Label(label_frame_2, text="")
+secilen_video_label.place(x=5, y=5)
+
+secilen_video_frame_rate_label = ttk.Label(label_frame_2, text="")
+secilen_video_frame_rate_label.place(x=5, y=35)
+
 
 
 window.mainloop()
