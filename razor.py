0aa82b426d94a1d4357f307cb3ffecfe76b730ca
Hasan YILMAZ <hasan.yilmaz@trt.net.tr>
2023-07-31 17:44:02 +0300

    duration yerine out timecode konuldu

--- razor.py
+++ razor.py
@@ -12,8 +12,8 @@ from ttkbootstrap.constants import *
 
 from tkinter import filedialog, Tk, Button
 
-window = ttk.Window(title="Razor by hsnylmz", themename="litera")
-window.geometry("600x280")
+window = ttk.Window(title="Razor by hsnylmz v1.1", themename="cyborg")
+window.geometry("700x300")
 window.resizable(True,True)
 
 try:
@@ -25,9 +25,31 @@ except:
 source_video_path=""
 target_video_file=""
 file_ext=""
-
+fps=""
 videos_folder = os.path.join(os.path.expanduser("~"), "Videos")
 yolumuz=os.getcwd()
+
+
+def timecode_to_frames(timecode,fps):
+    # Timecode'ı "HH:MM:SS.FF" formatından alıp saat, dakika, saniye ve frame bölümlerine ayır
+    time_parts = timecode.split(':')
+    hours, minutes, seconds, frame_s = int(time_parts[0]), int(time_parts[1]), int(time_parts[2]) , int(time_parts[3])
+    
+    # Toplam frame sayısını hesapla
+    total_frames = (hours * 3600 * fps ) + (minutes * 60 * fps)  + (seconds * fps ) +  frame_s
+
+    return total_frames
+
+def frames_to_timecode(frames,fps):
+    # Toplam frame sayısını "HH:MM:SS.FF" formatına dönüştür
+    hours = frames // (fps * 3600)
+    frames %= (fps * 3600)
+    minutes = frames // (fps * 60)
+    frames %= (fps * 60)
+    seconds = frames // fps
+    frames %= fps
+
+    return "{:02d}:{:02d}:{:02d}.{:02d}".format(int(hours), int(minutes), int(seconds), int(frames))
 
 ### frame rate alınması lazım...
 def get_frame_rate(source_video_path):
@@ -53,27 +75,27 @@ def get_file_extension(file_path):
     return file_extension
 
 def cevir():
-    global source_video_path, target_video_file
+    global source_video_path, target_video_file,fps
     
     source_video_path = filedialog.askopenfilename(initialdir=videos_folder)
         
     fps=get_frame_rate(source_video_path)
-    print('Frame Rate : '+str(fps))
+    #print('Frame Rate : '+ str(fps))
     
-    print('Dosya Yolu : '+source_video_path)
+    #print('Dosya Yolu : '+ source_video_path)
 
     file_ext = get_file_extension(source_video_path)
-    print('Kaynak dosya uzantısı : '+file_ext)
+    #print('Kaynak dosya uzantısı : '+ file_ext)
     
     source_file_name = source_video_path.split("/")[-1]
-    print('Source File Name :'+ source_file_name)
+    #print('Source File Name :'+ source_file_name)
 
     target_video_file = 'output_' + source_file_name
-    print('Target File Name : '+target_video_file)
+    #print('Target File Name : '+ target_video_file)
 
-    secilen_video_label.config(text=f"{source_file_name}", font=("Arial", 14),bootstyle="danger")
-    secilen_video_frame_rate_label.config(text=f"{fps}", font=("Arial", 14), bootstyle="danger")
-    target_video_file_isim.config(text=f"{target_video_file}", font=("Arial", 14), bootstyle="danger"),
+    secilen_video_label.config(text=f"{source_file_name}", font=("Arial", 14),bootstyle="PRIMARY")
+    secilen_video_frame_rate_label.config(text=f"{fps}", font=("Arial", 14), bootstyle="PRIMARY")
+    target_video_file_isim.config(text=f"{target_video_file}", font=("Arial", 14), bootstyle="PRIMARY"),
     dest_file_path_isim.config(text="")
 
     olarak_kaydet_button.config(state=NORMAL)
@@ -82,25 +104,36 @@ def cevir():
 
 def farkli_kaydet():
     
-    global file_ext, source_file_name, yolumuz
+    global file_ext, source_file_name, yolumuz, fps
     
     tc1=get_tc_1()
     tc_1=tc1.strip()
-    print('tc1 : '+tc_1)
+    #print('tc1 : '+tc_1)
 
     tc2=get_tc_2()
     tc_2=tc2.strip()
-    print('tc2 : '+tc_2)
+    #print('tc2 : '+tc_2)
     
     dest_file_path = filedialog.askdirectory()
-    print('dest_file_path : '+ dest_file_path)
+    #print('dest_file_path : '+ dest_file_path)
 
-    dest_file_path_isim.config(text=f"{dest_file_path}", font=("Arial", 14), bootstyle="danger")
-
+    dest_file_path_isim.config(text=f"{dest_file_path}", font=("Arial", 14), bootstyle="PRIMARY")
     
-    convert_command='{}/ffmpeg.exe -ss {} -i "{}" -c copy -t {} "{}/{}"'.format(yolumuz, tc_1, source_video_path, tc_2, dest_file_path, target_video_file)
-    print('-')
-    print('burası senin son uğraştığın yer : '+convert_command)
+
+    timecode_in= timecode_to_frames(tc_1, fps) # duration hesabı yapmak için tcleri frame e dönültürme fonksiyonuna gönderme
+    timecode_out = timecode_to_frames(tc_2, fps) # duration hesabı yapmak için tcleri frame e dönültürme fonksiyonuna gönderme
+    
+    duration_frames = timecode_out - timecode_in #durationu bulmak için in out tc framelerini çıkarma işlemi
+
+    in_timecode_format = frames_to_timecode(abs(timecode_in),fps) # kutuda : ile olan in tc yi . ile değiştirmek için.
+    
+    duration_timecode = frames_to_timecode(abs(duration_frames),fps)
+
+    duration_lbl.config(text=f"{duration_timecode}", font=("Arial", 14), bootstyle="PRIMARY")
+    
+    convert_command='{}/ffmpeg.exe -ss {} -i "{}" -c copy -t {} "{}/{}"'.format(yolumuz, in_timecode_format, source_video_path, duration_timecode, dest_file_path, target_video_file)
+    #print('-')
+    #print('burası senin son uğraştığın yer : '+convert_command)
 
 
     os.system(convert_command)
@@ -130,7 +163,6 @@ def get_tc_2():
     print(text_tc_2)
     return text_tc_2
 
-
 # USER INTERFACE
 label_frame = ttk.LabelFrame(window, text="İşleminizi Seçiniz : ")
 label_frame.pack(padx=5, pady=5)
@@ -138,10 +170,10 @@ label_frame.pack(padx=5, pady=5)
 label_frame_2 = ttk.LabelFrame(window, text="Video Özellikleri ")
 label_frame_2.pack(padx=5, pady=5, fill=ttk.BOTH, expand=True)
 
-head_label_tc1 = ttk.Label(label_frame, text="TC IN")
+head_label_tc1 = ttk.Label(label_frame, text="IN (HH:MM:SS:FF)")
 head_label_tc1.grid(row=0, column=1, padx=5, pady=5)
 
-head_label_tc2 = ttk.Label(label_frame, text="TC DURATION")
+head_label_tc2 = ttk.Label(label_frame, text="OUT (HH:MM:SS:FF) ")
 head_label_tc2.grid(row=0, column=2, padx=5, pady=5)
 
 
@@ -149,20 +181,20 @@ convert_button = ttk.Button(label_frame, text="VİDEOYU SEÇİNİZ", command=cev
 #convert_button.pack(side='left', padx=5, pady=5)
 convert_button.grid(row=1, column=0, padx=5, pady=5)
 
-text_box_tc_1 = ttk.Text(label_frame, height=1, width=10,font=("Arial", 14))
-text_box_tc_1.insert("1.0", "00:00:00.0")
+text_box_tc_1 = ttk.Text(label_frame, height=1, width=14,font=("Arial", 14))
+text_box_tc_1.insert("1.0", "00:00:00:0")
 #text_box_tc_1.pack(side='left', padx=5, pady=5)
 text_box_tc_1.grid(row=1, column=1, padx=5, pady=5)
 
 #label_tc2=ttk.Label(label_frame,text="TIMECODE DURATION : ")
 #label_tc2.grid(row=4, column=0, padx=5, pady=5)
 
-text_box_tc_2 = ttk.Text(label_frame, height=1, width=10,font=("Arial", 14))
-text_box_tc_2.insert("1.0", "00:00:10.0")
+text_box_tc_2 = ttk.Text(label_frame, height=1, width=14,font=("Arial", 14))
+text_box_tc_2.insert("1.0", "00:00:25:0")
 #text_box_tc_2.pack(side='left', padx=5, pady=5)
 text_box_tc_2.grid(row=1, column=2, padx=5, pady=5)
 
-olarak_kaydet_button = ttk.Button(label_frame, text="K A Y D E T", command=farkli_kaydet, width=18, bootstyle=SUCCESS)
+olarak_kaydet_button = ttk.Button(label_frame, text="K A Y D E T", command=farkli_kaydet, width=18, bootstyle=WARNING)
 olarak_kaydet_button.config(state=DISABLED)
 #olarak_kaydet_button.pack(side='left', padx=5, pady=5)
 olarak_kaydet_button.grid(row=1, column=3, padx=5, pady=5)
@@ -183,6 +215,11 @@ dest_file_name_label = ttk.Label(label_frame_2, text="Kaydedilecek Dosya yolu : 
 dest_file_name_label.config(font=("Arial",14))
 dest_file_name_label.place(x=5, y=95)
 
+duration_lbl_title = ttk.Label(label_frame_2, text="Duration (video Süresi) : ")
+duration_lbl_title.config(font=("Arial",14))
+duration_lbl_title.place(x=5, y=125)
+
+
 secilen_video_label = ttk.Label(label_frame_2, text="")
 secilen_video_label.place(x=160, y=5)
 
@@ -194,6 +231,10 @@ target_video_file_isim.place(x=250,y=65)
 
 dest_file_path_isim = ttk.Label(label_frame_2, text="")
 dest_file_path_isim.place(x=250,y=95)
+
+duration_lbl = ttk.Label(label_frame_2, text="")
+duration_lbl.place(x=250,y=125)
+
 
 
 window.mainloop()
